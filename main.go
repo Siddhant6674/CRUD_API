0d77f56f76b15f93ca70e67c9ff3192f3fecbf7c
iNJUSTiCE <injusticenubs@gmail.com>
2024-12-05 19:59:25 +0530

    My changes

--- main.go
+++ main.go
@@ -78,6 +78,21 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+func updateMovieVersion1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+
+	for index, _ := range movies {
+		if movies[index].ID == params["id"] {
+		_ = json.NewDecoder(r.Body).Decode(&movies[index])
+			movies[index].ID = params["id"]
+			json.NewEncoder(w).Encode(movies[index])
+
+		}
+	}
+}
+
 func main() {
 
 	r := mux.NewRouter()
@@ -85,11 +100,15 @@ func main() {
 	movies = append(movies, Movie{ID: "1", Isbn: "435673", Title: "Movie one", Director: &Director{FirstName: "Somesh", LastName: "Kanitkar"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "423567", Title: "Movie two", Director: &Director{FirstName: "Ashish", LastName: "Pawar"}})
 	movies = append(movies, Movie{ID: "3", Isbn: "462776", Title: "Movie Three", Director: &Director{FirstName: "Rajat", LastName: "Achalkar"}})
+	// for i:=4;i<1000000;i=i+1{
+	// 	movies = append(movies, Movie{ID: "4", Isbn: "462776", Title: "Movie Three", Director: &Director{FirstName: "Rajat", LastName: "Achalkar"}})
+	// }
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/moviesNewMethod/{id}", updateMovieVersion1).Methods("PUT")
 	r.HandleFunc("movies/{id}", deleteMovie).Methods("DELETE")
 	fmt.Printf("Server running on port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
